1ecf920c0132ed7743611a8b965e0769d108247c
dkds <dkds@users.noreply.github.com>
2026-01-05 23:38:51 +0530

    Add normal doubling function

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
-package functions2
+package main
 
+import "fmt"
 
-func main()  {
-	
+func main() {
+	numbers := []int{1, 2, 3, 4}
+
+	doubled := doubleNumbers(&numbers)
+	fmt.Println(doubled)
+}
+
+func doubleNumbers(numbers *[]int) []int {
+	doubled := []int{}
+	for number := range *numbers {
+		doubled = append(doubled, number*2)
+	}
+	return doubled
 }
