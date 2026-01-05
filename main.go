c831d1cf9875c2a45488335e81541e237b654a33
dkds <dkds@users.noreply.github.com>
2026-01-05 23:51:12 +0530

    Use function as a value to pass behavior to functions

--- main.go
+++ main.go
@@ -4,15 +4,34 @@ import "fmt"
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	fmt.Println("Numbers: ", numbers)
 
-	doubled := doubleNumbers(&numbers)
-	fmt.Println(doubled)
+	doubled := transformNumbers(&numbers, double)
+	fmt.Println("Doubled: ", doubled)
+
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println("Tripled: ", tripled)
+
+	sqred := transformNumbers(&numbers, sqr)
+	fmt.Println("Squared: ", sqred)
 }
 
-func doubleNumbers(numbers *[]int) []int {
+func transformNumbers(numbers *[]int, transformation func(int) int) []int {
 	doubled := []int{}
 	for number := range *numbers {
-		doubled = append(doubled, number*2)
+		doubled = append(doubled, transformation(number))
 	}
 	return doubled
+}
+
+func double(num int) int {
+	return num * 2
+}
+
+func triple(num int) int {
+	return num * 3
+}
+
+func sqr(num int) int {
+	return num * num
 }
