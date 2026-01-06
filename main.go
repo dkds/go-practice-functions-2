abb407015b5263069749f17aeac24464871e1132
dkds <dkds@users.noreply.github.com>
2026-01-06 22:58:31 +0530

    Use recursion to calculate factorial

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ func main() {
 	fmt.Println("Factorial of", number, "is", factorial)
 }
 
+// func factorial(number int) int {
+// 	total := 1
+// 	for i := 1; i <= number; i++ {
+// 		total *= i
+// 	}
+// 	return total
+// }
+
 func factorial(number int) int {
-	total := 1
-	for i := 1; i <= number; i++ {
-		total *= i
+	if number == 0 {
+		return 1
 	}
-	return total
+	return number * factorial(number-1)
 }
