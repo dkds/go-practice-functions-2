6bd0ee4656064a5952ef381110e66d3710b343ca
dkds <dkds@users.noreply.github.com>
2026-01-06 23:18:23 +0530

    Use variadic function to capture function args as an array

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{1, 2, 3, 4, 5}
-	sum := sum(numbers)
-	fmt.Println("Sum of", numbers, "is", sum)
+	sum := sum(1, 2, 3, 4, 5)
+	fmt.Println("Sum is", sum)
 }
 
-func sum(numbers []int) int {
+func sum(numbers ...int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
